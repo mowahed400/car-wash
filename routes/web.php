d89788477bbde09b04bb058fa4d6817e4e9e1c6f
MohamedElsaidy100 <105368662+MohamedElsaidy100@users.noreply.github.com>
2023-08-07 18:42:52 +0300

    auth to dashboard

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath','auth' ]
+    ], function(){
+
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 });
