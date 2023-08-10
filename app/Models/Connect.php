309e39fee239921642af66f3f6f56d819d1b8a3c
mowahed400 <60117806+mowahed400@users.noreply.github.com>
2023-08-10 19:56:02 +0300

    spatie

--- app/Models/Connect.php
+++ app/Models/Connect.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Connect extends Model
 {
+    use HasTranslations;
     use HasFactory;
     protected $guarded;
 }
